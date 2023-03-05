55acb4bb10c7de380be34c3b3a46a4de934e38f6
Rahul Rudra <rahulrudra665@gmail.com>
2023-03-05 07:20:47 +0600

    first commit

---
+++ src/Components/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Welcome To Rest Countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
