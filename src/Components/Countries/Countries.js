543718aed2ab0c211bc5273be684bb14c04f2b0e
Rahul Rudra <rahulrudra665@gmail.com>
2023-03-05 07:31:42 +0600

    new commit

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+
+    const [countries,setCountries]=useState([]);
+
+    useEffect(()=>{
+
+        fetch("https://restcountries.com/v3.1/all").then(res=>res.json()).then(data=>setCountries(data))
+    },[]);
+   
     return (
+        
         <div>
             <h1>Welcome To Rest Countries</h1>
+            <p>Available Countries={countries.length}</p>
+            {
+
+
+            countries.map(country=>console.log(country))
+            }
         </div>
     );
 };
